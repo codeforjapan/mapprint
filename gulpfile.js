76a7b73054d8ebeaa32f8522a698c3d447fbd9a4
Hal Seki <hal@plants-web.jp>
2018-07-10 21:27:17 +0900

    add scss support

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,45 @@
 var gulp = require('gulp');
 var source = require('vinyl-source-stream');
 var browserify = require('browserify');
+var sass = require('gulp-sass');
+var neat = require('node-neat');
 
-var paths = {
+var jsConf = {
     srcPath: 'source/javascripts/all.js',
     destFileName: 'javascripts/bundle.js',
-    destPath: '.tmp/dist'
+    destPath: '.tmp/dist/'
+}
+
+var cssConf = {
+    srcPath: 'source/stylesheets/**/*.scss',
+    destFileName: 'site',
+    destPath: '.tmp/dist/stylesheets'
 }
 
 var b = browserify({
-    entries: paths.srcPath,
+    entries: jsConf.srcPath,
     cache: {},
     packageCache: {}
 });
 
 gulp.task('default', ['build']);
-gulp.task('build', bundle);
+gulp.task('build', ['sass', 'bundle']);
 gulp.task('watch', function(){
   //
 });
+gulp.task('bundle', jsBundle);
+gulp.task('sass', sassPreCompile);
 
-function bundle() {
+function jsBundle() {
     return b.bundle()
-    .pipe(source(paths.destFileName))
-        .pipe(gulp.dest(paths.destPath));
+    .pipe(source(jsConf.destFileName))
+        .pipe(gulp.dest(jsConf.destPath));
+}
+
+function sassPreCompile(){
+  gulp.src(cssConf.srcPath)
+    .pipe(sass({
+        includePaths: cssConf.destFileName
+    }))
+    .pipe(gulp.dest(cssConf.destPath));
 }

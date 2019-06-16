be3fdaf21121354a46faf593225bd5f3463f1302
Hal Seki <hal@plants-web.jp>
2019-06-16 18:35:08 +0900

    add nodemon

--- spec/mapHelperSpec.ts
+++ spec/mapHelperSpec.ts
@@ -1,7 +1,7 @@
 import mapHelper = require('../source/javascripts/mapHelper');
 
 describe('tileServerAttribution', () => {
-  it('returns openstreetmap license', function() {
+  it('returns openstreetmap license for testing environment', function() {
     expect(mapHelper.tileServerAttribution('localhost:4567')).toBe('Map data © <a href="http://openstreetmap.org/">OpenStreetMap</a>');
   });
 });

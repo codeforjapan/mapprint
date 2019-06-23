d23d0564f5f9382a67cc905b5d4f58d6f5152cf4
Hal Seki <hal@plants-web.jp>
2019-06-23 23:33:30 +0900

    add new source to html

--- source/javascripts/site.ts
+++ source/javascripts/site.ts
@@ -1,1 +1,9 @@
-// This is where it all goes :)
+import * as $ from 'jquery';
+import PrintableMap from './mapHelper';
+
+$(function(){
+  if ($('#map')){
+    let map = new PrintableMap("localhost:4567", "map");
+    map.loadFile('./images/data.umap');
+  }
+});

35b05b1f3a45ba250b972b0081d708fa94da2368
Taro Matsuzawa <taro@georepublic.co.jp>
2019-09-16 17:09:28 +0900

    #181 enable to change map style

--- source/javascripts/site.ts
+++ source/javascripts/site.ts
@@ -1,9 +1,11 @@
 import * as $ from 'jquery';
-import PrintableMap from './mapHelper';
+import PrintableMap, { IPrintableMap } from './mapHelper';
+
 
 $(function(){
+  let map: IPrintableMap;
   if ($('#map')){
-    let map = new PrintableMap(window.location.hostname, "map");
+    map = new PrintableMap(window.location.hostname, "map");
     map.loadFile('./images/chiba.kml');
   }
   $('#print').on('click', () => {
@@ -17,5 +19,10 @@ $(function(){
     } else {
       $('#close').text('閉じる')
     }
+  });
+
+  $('input[name="mapStyle"]:radio').change( function() {
+    const mapStyle:string = <string> $(this).val();
+    map.changeStyle(mapStyle,window.location.hostname)
   });
 });

572d184510fac450ad082b5b23fea877a5bd45fe
Hal Seki <hal@Hals-MacBook-Pro.local>
2020-02-16 10:36:06 +0900

    export vector tiles

--- create_tiles.sh
+++ create_tiles.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "convert $1 into tiles"
+path=$1
+dir=${path%/*}
+file=${path##*/}
+id=${file%".kml"}
+target="${id}.mbtiles"
+zxy="${id}/zxy"
+mkdir -p "${dir}/$zxy"
+./node_modules/togeojson/togeojson "${dir}/$file" | tippecanoe  -f -o "${dir}/$target" --base-zoom=2 
+tile-join --output-to-directory="${dir}/$zxy" --force --no-tile-compression --no-tile-size-limit "${dir}/$target"
--- createtiles.ts
+++ createtiles.ts
@@ -4,33 +4,38 @@ const shell = require('shelljs')
 
 const TARGET_DIR = './dist/data/';
 const CONFIG_DIR = './assets/config/'
+
+// create target directory
 if (!fs.existsSync(TARGET_DIR)) {
     fs.mkdirSync(TARGET_DIR, {recursive: true});
 }
 
+// load config
 const list = require(`${CONFIG_DIR}list.json`)
 list.map((name) => {
+  // load map config
   console.log(`loading ${CONFIG_DIR}${name}`)
   const config = require(`${CONFIG_DIR}${name}`)
   config.sources.forEach((source) => {
+    // download source file
     if (!fs.existsSync(`${TARGET_DIR}${config.map_id}`)) {
       fs.mkdirSync(`${TARGET_DIR}${config.map_id}`, {recursive: true});
     }
     console.log(`downloading ${source.url}...`)
     axios.get(source.url).then((response) => {
+      // currently, this is supporting only kml
       const file_path = `${TARGET_DIR}${config.map_id}/${source.id}.${source.type}`
       fs.writeFile(file_path, response.data, (err) => {
-        // 書き出しに失敗した場合
+        // failed
         if(err){
           console.log("Downloading kml file failed" + err)
           throw err
         }
-        // 書き出しに成功した場合
+        // success
         else{
           console.log(`Downloaded ${source.id}.${source.type}`)
-          console.log(`./create_tiles.sh ${file_path}`)
+          // convert kml files to xyz tile
           shell.exec(`./create_tiles.sh ${file_path}`)
-          // shell.exec(`./convert_to_tiles.sh ${file_path}`)
         }
       })
     })

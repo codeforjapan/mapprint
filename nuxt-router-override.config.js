1c39649c6af3b78a857ea2bfab508ef10bf2047a
yuiseki <yuiseki@gmail.com>
2021-03-27 06:07:21 +0000

    overrideをコミットしてしまっていた

--- nuxt-router-override.config.js
+++
@@ -1,7 +0,0 @@
-// for forked project
-// copy this file to nuxt-router-override.config.js
-// will effect override nuxt.config.js router configuration
-// cp nuxt-router-override-sample.config.js nuxt-router-override.config.js
-export default {
-  base: '/mapprint'
-}

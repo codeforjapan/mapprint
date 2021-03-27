a9f189ce9482e1992737d90c736db95929bd4865
yuiseki <yuiseki@gmail.com>
2021-03-27 04:19:25 +0000

    言語選択をプルダウンメニューにする

--- pages/map/_map.vue
+++ pages/map/_map.vue
@@ -50,12 +50,13 @@ div.layout-map
               .sub-button.github-link
                 i.fab.fa-github.fa-lg
                 a(href="https://github.com/codeforjapan/mapprint") {{ $t('common.contribute') }}
-              .sub-button(v-for="locale in $i18n.locales")
-                nuxt-link(
-                  :key="locale.code"
-                  :to="switchLocalePath(locale.code)"
-                )
-                  span {{ locale.name }}
+              .sub-button
+                i.fas.fa-language.fa-lg
+                select(onChange="location.href=value;")
+                  option.language(disabled selected) 
+                    | Language: {{$i18n.locales.filter((i) => { return i.code === $i18n.locale })[0].name}}
+                  option(v-for="locale in $i18n.locales" :value="switchLocalePath(locale.code)")
+                    | {{ locale.name }}
             .title-outer
               h1.title(v-if="map_config && $i18n.locale === 'ja'")
                 | {{map_config.map_title}}

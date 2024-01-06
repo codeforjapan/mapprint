a9aa3bbb86b7bcba2779223f436bcfaee8aca3e9
ItsukiKigoshi <itsukikigoshi@gmail.com>
2024-01-07 01:05:28 +0900

    Undo indent in components/Modal.vue

--- components/Modal.vue
+++ components/Modal.vue
@@ -16,39 +16,39 @@ div
 </template>
 
 <script lang="js">
-  export default {
-    props: {
-      isOpen: {
-        type: Boolean,
-        default: false
+export default {
+  props: {
+    isOpen: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    // Languages other than Japanese are automatically changed to English.
+    // Once you support all the languages, please just put "let about = this.$t('about')" here.
+    let about;
+    switch (this.$i18n.locale) {
+        case "ja":
+          about = this.$t('about')
+          break;
+        default:
+          about = {
+            "desc_1": "This site is open source. If you want to contribute to this project, please visit the ",
+            "desc_2": "Code for Japan's Github repository",
+            "desc_3": ". Everyone is welcome, and we especially invite those with JavaScript or Leaflet experience to join us."
+          };
+          break;
       }
-    },
-    data() {
-      // Languages other than Japanese are automatically changed to English.
-      // Once you support all the languages, please just put "let about = this.$t('about')" here.
-      let about;
-      switch (this.$i18n.locale) {
-          case "ja":
-            about = this.$t('about')
-            break;
-          default:
-            about = {
-              "desc_1": "This site is open source. If you want to contribute to this project, please visit the ",
-              "desc_2": "Code for Japan's Github repository",
-              "desc_3": ". Everyone is welcome, and we especially invite those with JavaScript or Leaflet experience to join us."
-            };
-            break;
-        }
-      return {
-        mapConfig: this.$nuxt.$route.params.map ? require('~/assets/config/' + (this.$nuxt.$route.params.map)) : '',
-        about,
-      }
-    },
-    methods: {
-      handleClick () {
-        this.$emit('update:isOpen', false)
-        this.$emit('closeModal')
-      }
+    return {
+      mapConfig: this.$nuxt.$route.params.map ? require('~/assets/config/' + (this.$nuxt.$route.params.map)) : '',
+      about,
+    }
+  },
+  methods: {
+    handleClick () {
+      this.$emit('update:isOpen', false)
+      this.$emit('closeModal')
     }
   }
+}
 </script>

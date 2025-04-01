9f475bc9ab25362de0c657d2c25834f3f3fcbdd8
Hal Seki <hal@plants-web.jp>
2025-04-01 20:58:55 +0900

    add i18n module

--- nuxt3-migration/nuxt.config.ts
+++ nuxt3-migration/nuxt.config.ts
@@ -3,14 +3,47 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  app: {
+    head: {
+      meta: [
+        { charset: 'utf-8' },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+        { property: 'og:type', content: 'website' },
+        { property: 'og:image', content: 'https://kamimap.com/images/ogp_main.png' },
+        { name: 'twitter:card', content: 'summary_large_image' }
+      ],
+      link: [
+        { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+        { rel: 'apple-touch-icon', href: '/apple-touch-icon.png' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Open+Sans' }
+      ]
+    }
+  },
+
   modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/fonts',
-    '@nuxt/image',
-    '@nuxt/icon',
-    '@nuxt/scripts',
-    '@nuxt/ui',
-    '@nuxt/test-utils'
-  ]
+    '@nuxtjs/i18n'
+  ],
+
+  i18n: {
+    strategy: 'prefix_except_default',
+    defaultLocale: 'ja',
+    locales: ['ja', 'en']
+  },
+
+  css: [
+    // Will add Font Awesome later
+    // '@/assets/fonts/fontawesome/css/all.css',
+    // Will add SASS styles later
+    // '@/assets/sass/styles.scss',
+  ],
+
+  vite: {
+    resolve: {
+      alias: {
+        'mapbox-gl': 'maplibre-gl'
+      }
+    }
+  },
+
+  compatibilityDate: '2025-04-01'
 })
